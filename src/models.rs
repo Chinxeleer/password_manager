b2780d46806ac4b728ec56b6b3bdab796421aaee
autumn_leaf <blessingeleer.programming@gmail.com>
2023-09-28 19:19:20 +0200

    updated the models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,10 @@
 use crate::schema::accounts;
 
 #[derive(Insertable)]
-#[table_name="accounts"]
-pub struct NewAccount<'a>{
-    pub username:&'a str,
-    pub email_address:&'a str,
-    pub password:&'a str,
-    pub in_use_by:&'a str
+#[table_name = "accounts"]
+pub struct NewAccount<'a> {
+    pub username: &'a str,
+    pub email_address: &'a str,
+    pub password: &'a str,
+    pub in_use_by: Vec<&'a str>,
 }
